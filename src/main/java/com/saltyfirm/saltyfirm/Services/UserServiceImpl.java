3ac33039b3c804be906d8283c1d2f63cfc7efe8b
Nmtur <n.m.tureczek@gmail.com>
2019-12-06 11:04:54 +0100

    my commit

--- src/main/java/com/saltyfirm/saltyfirm/Services/UserServiceImpl.java
+++ src/main/java/com/saltyfirm/saltyfirm/Services/UserServiceImpl.java
@@ -1,8 +1,38 @@
 package com.saltyfirm.saltyfirm.Services;
 
+import com.saltyfirm.saltyfirm.Models.User;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+
+
+    @Override
+    public int editUser(int userId, String username, String password, String firstname, String lastname, int phoneNumber, String gender, Date birthdate, String education, String mail, String nationality, String priviliges) {
+        return 0;
+    }
+
+    @Override
+    public int deleteUser(int userId) {
+        return 0;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public int findUserById(int userId) {
+        return 0;
+    }
+
+    @Override
+    public User checkLogin(String user) {
+        return null;
+    }
 }
