30a415359f022cc843a06ef865f9e645ce1bd9dd
cycloniccornet <patr5152@hotmail.com>
2019-12-06 09:41:21 +0100

    my commit

--- src/main/java/com/saltyfirm/saltyfirm/Models/Review.java
+++ src/main/java/com/saltyfirm/saltyfirm/Models/Review.java
@@ -1,4 +1,101 @@
 package com.saltyfirm.saltyfirm.Models;
 
 public class Review {
+
+    private int reviewId;
+    private String post;
+    private int salary;
+    private String position;
+    private int pentionScheme;
+    private int benefits;
+    private int management;
+    private int workEnvironment;
+    private int flexibility;
+
+    public Review(int reviewId, String post, int salary, String position, int pentionScheme, int benefits, int management, int workEnvironment, int flexibility) {
+        this.reviewId = reviewId;
+        this.post = post;
+        this.salary = salary;
+        this.position = position;
+        this.pentionScheme = pentionScheme;
+        this.benefits = benefits;
+        this.management = management;
+        this.workEnvironment = workEnvironment;
+        this.flexibility = flexibility;
+    }
+
+    public Review() {
+    }
+
+    public int getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(int reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getPentionScheme() {
+        return pentionScheme;
+    }
+
+    public void setPentionScheme(int pentionScheme) {
+        this.pentionScheme = pentionScheme;
+    }
+
+    public int getBenefits() {
+        return benefits;
+    }
+
+    public void setBenefits(int benefits) {
+        this.benefits = benefits;
+    }
+
+    public int getManagement() {
+        return management;
+    }
+
+    public void setManagement(int management) {
+        this.management = management;
+    }
+
+    public int getWorkEnvironment() {
+        return workEnvironment;
+    }
+
+    public void setWorkEnvironment(int workEnvironment) {
+        this.workEnvironment = workEnvironment;
+    }
+
+    public int getFlexibility() {
+        return flexibility;
+    }
+
+    public void setFlexibility(int flexibility) {
+        this.flexibility = flexibility;
+    }
 }
