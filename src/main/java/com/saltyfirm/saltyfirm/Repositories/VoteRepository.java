2bcb8d875d294b6240bbf72a39af7edce04ac7a3
victorwp288 <victorwpetersen@gmail.com>
2019-12-06 10:58:35 +0100

    Updated shell

--- src/main/java/com/saltyfirm/saltyfirm/Repositories/VoteRepository.java
+++ src/main/java/com/saltyfirm/saltyfirm/Repositories/VoteRepository.java
@@ -1,4 +1,8 @@
 package com.saltyfirm.saltyfirm.Repositories;
 
 public interface VoteRepository {
+
+    int voteReview(boolean vote, int reviewId, int userId);
+
+    int removeVote(int reviewId, int userId);
 }
