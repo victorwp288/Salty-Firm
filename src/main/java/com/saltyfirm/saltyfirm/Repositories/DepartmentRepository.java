73d65856ae37d01e2ff2d27db38878b4f1471898
cycloniccornet <patr5152@hotmail.com>
2019-12-06 10:59:34 +0100

    my commit

--- src/main/java/com/saltyfirm/saltyfirm/Repositories/DepartmentRepository.java
+++ src/main/java/com/saltyfirm/saltyfirm/Repositories/DepartmentRepository.java
@@ -1,4 +1,14 @@
 package com.saltyfirm.saltyfirm.Repositories;
 
+import com.saltyfirm.saltyfirm.Models.Department;
+
+import java.util.List;
+
 public interface DepartmentRepository {
+
+    int findDepartmentById(int departmentId);
+    List<Department> getDepartments(int firmId);
+    int deleteDepartment(int departmentId);
+    int editDepartment(int departmentId, String departmentName, String departmentAdress, double departmentScore);
+    double calculateDepartmentScore(int departmentId);
 }
--- src/main/java/com/saltyfirm/saltyfirm/Repositories/ReviewRepository.java
+++ src/main/java/com/saltyfirm/saltyfirm/Repositories/ReviewRepository.java
@@ -1,4 +1,8 @@
 package com.saltyfirm.saltyfirm.Repositories;
 
 public interface ReviewRepository {
+
+    int createReview(int userId, int reviewId, String post, int salary, String position, int pentionScheme, int benefits, int management, int workEnvironment, int flexibility);
+    int editReview(int userId, int reviewId, String post, int salary, String position, int pentionScheme, int benefits, int management, int workEnvironment, int flexibility);
+    int deleteReview(int reviewId);
 }
