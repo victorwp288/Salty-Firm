5c01f9bec24627c8059b08063ef37865016089a3
Nmtur <n.m.tureczek@gmail.com>
2019-12-06 11:08:34 +0100

    my commit

--- src/main/java/com/saltyfirm/saltyfirm/Repositories/ReviewRepository.java
+++ src/main/java/com/saltyfirm/saltyfirm/Repositories/ReviewRepository.java
@@ -2,7 +2,7 @@ package com.saltyfirm.saltyfirm.Repositories;
 
 public interface ReviewRepository {
 
-    int createReview(int userId, int reviewId, String post, int salary, String position, int pentionScheme, int benefits, int management, int workEnvironment, int flexibility);
-    int editReview(int userId, int reviewId, String post, int salary, String position, int pentionScheme, int benefits, int management, int workEnvironment, int flexibility);
+    int createReview(int userId, int reviewId, String post, int salary, String position, int pensionScheme, int benefits, int management, int workEnvironment, int flexibility);
+    int editReview(int userId, int reviewId, String post, int salary, String position, int pensionScheme, int benefits, int management, int workEnvironment, int flexibility);
     int deleteReview(int reviewId);
 }
--- src/main/java/com/saltyfirm/saltyfirm/Services/ReviewServiceImpl.java
+++ src/main/java/com/saltyfirm/saltyfirm/Services/ReviewServiceImpl.java
@@ -5,12 +5,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReviewServiceImpl implements ReviewService {
     @Override
-    public int createReview(int userId, int reviewId, String post, int salary, String position, int pentionScheme, int benefits, int management, int workEnvironment, int flexibility) {
+    public int createReview(int userId, int reviewId, String post, int salary, String position, int pensionScheme, int benefits, int management, int workEnvironment, int flexibility) {
         return 0;
     }
 
     @Override
-    public int editReview(int userId, int reviewId, String post, int salary, String position, int pentionScheme, int benefits, int management, int workEnvironment, int flexibility) {
+    public int editReview(int userId, int reviewId, String post, int salary, String position, int pensionScheme, int benefits, int management, int workEnvironment, int flexibility) {
         return 0;
     }
 
