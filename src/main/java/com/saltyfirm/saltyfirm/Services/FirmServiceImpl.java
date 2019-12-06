fb428984533631512705a2c06a1651405b6741c7
Nmtur <n.m.tureczek@gmail.com>
2019-12-06 11:07:05 +0100

    my commit

--- src/main/java/com/saltyfirm/saltyfirm/Services/FirmServiceImpl.java
+++ src/main/java/com/saltyfirm/saltyfirm/Services/FirmServiceImpl.java
@@ -4,4 +4,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FirmServiceImpl implements FirmService {
+    @Override
+    public String searchFirms(String word) {
+        return null;
+    }
+
+    @Override
+    public int findFirmById(int firmId) {
+        return 0;
+    }
+
+    @Override
+    public int deleteFirm(int firmId) {
+        return 0;
+    }
+
+    @Override
+    public int editFirm(int firmId, String firmName, String firmType, double overallScore, String description, String logoURL) {
+        return 0;
+    }
 }
