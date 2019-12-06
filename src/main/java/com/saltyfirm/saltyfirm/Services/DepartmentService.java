68a9a7fe19c1e176794d6aabae4448f27fb52d83
cycloniccornet <patr5152@hotmail.com>
2019-12-06 11:05:22 +0100

    my commit

--- src/main/java/com/saltyfirm/saltyfirm/Services/DepartmentService.java
+++ src/main/java/com/saltyfirm/saltyfirm/Services/DepartmentService.java
@@ -1,4 +1,14 @@
 package com.saltyfirm.saltyfirm.Services;
 
+import com.saltyfirm.saltyfirm.Models.Department;
+
+import java.util.List;
+
 public interface DepartmentService {
+
+    int findDepartmentById(int departmentId);
+    List<Department> getDepartments(int firmId);
+    int deleteDepartment(int departmentId);
+    int editDepartment(int departmentId, String departmentName, String departmentAdress, double departmentScore);
+    double calculateDepartmentScore(int departmentId);
 }
--- src/main/java/com/saltyfirm/saltyfirm/Services/DepartmentServiceImpl.java
+++ src/main/java/com/saltyfirm/saltyfirm/Services/DepartmentServiceImpl.java
@@ -1,7 +1,39 @@
 package com.saltyfirm.saltyfirm.Services;
 
+import com.saltyfirm.saltyfirm.Models.Department;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
+
+    @Autowired
+
+
+    @Override
+    public int findDepartmentById(int departmentId) {
+        return 0;
+    }
+
+    @Override
+    public List<Department> getDepartments(int firmId) {
+        return null;
+    }
+
+    @Override
+    public int deleteDepartment(int departmentId) {
+        return 0;
+    }
+
+    @Override
+    public int editDepartment(int departmentId, String departmentName, String departmentAdress, double departmentScore) {
+        return 0;
+    }
+
+    @Override
+    public double calculateDepartmentScore(int departmentId) {
+        return 0;
+    }
 }
