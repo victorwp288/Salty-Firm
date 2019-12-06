dade9bcf005e668c408dfe75a986f16bcfd19ecb
Nmtur <n.m.tureczek@gmail.com>
2019-12-06 11:01:29 +0100

    my commit

--- src/main/java/com/saltyfirm/saltyfirm/Repositories/UserRepository.java
+++ src/main/java/com/saltyfirm/saltyfirm/Repositories/UserRepository.java
@@ -1,4 +1,16 @@
 package com.saltyfirm.saltyfirm.Repositories;
 
+import com.saltyfirm.saltyfirm.Models.User;
+
+import java.util.Date;
+import java.util.List;
+
 public interface UserRepository {
+
+    int editUser(int userId, String username, String password, String firstname, String lastname, int phoneNumber, String gender, Date birthdate, String education, String mail, String nationality, String priviliges);
+    int deleteUser(int userId);
+    List<User> getAllUsers();
+    int findUserById(int userId);
+    User checkLogin(String user);
+
 }
