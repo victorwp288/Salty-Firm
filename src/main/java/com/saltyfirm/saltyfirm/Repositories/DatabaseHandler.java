87aa47418d1d3d5d66642edc145bca9ab741842b
cycloniccornet <patr5152@hotmail.com>
2019-12-06 09:59:57 +0100

    my commit

---
+++ src/main/java/com/saltyfirm/saltyfirm/Repositories/DatabaseHandler.java
@@ -0,0 +1,14 @@
+package com.saltyfirm.saltyfirm.Repositories;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class DatabaseHandler {
+
+    private String springDatasourceUrl = "jdbc:mysql://saltyfirmdb.cxgtaawn9fts.eu-central-1.rds.amazonaws.com:3306";
+
+    private String dbUsername = "SaltyFirm";
+
+    private String dbPassword = "SaltyFirm";
+
+
+}
