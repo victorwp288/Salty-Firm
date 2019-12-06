f5579a1a565a71592267e946c30c7109af8be4be
victorwp288 <victorwpetersen@gmail.com>
2019-12-06 11:07:37 +0100

    Updated shell

--- src/main/java/com/saltyfirm/saltyfirm/Services/VoteServiceImpl.java
+++ src/main/java/com/saltyfirm/saltyfirm/Services/VoteServiceImpl.java
@@ -4,4 +4,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VoteServiceImpl implements VoteService {
+
+    @Override
+    public int voteReview(boolean vote, int reviewId, int userId){
+
+        return 0;
+    }
+
+    @Override
+    public int removeVote(int reviewId, int userId){
+
+        return 0;
+    }
 }
