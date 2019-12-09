2f3f7376adb0f67ecc72b0422eb086c2c39cd0c1
victorwp288 <victorwpetersen@gmail.com>
2019-12-09 13:55:27 +0100

    Updated shell

--- src/main/java/com/saltyfirm/saltyfirm/Repositories/FirmRepositoryImpl.java
+++ src/main/java/com/saltyfirm/saltyfirm/Repositories/FirmRepositoryImpl.java
@@ -73,12 +73,12 @@ public class FirmRepositoryImpl implements FirmRepository {
 
         try{
             Connection connection = DriverManager.getConnection(ProjectVariables.getUrl(), ProjectVariables.getUsername(), ProjectVariables.getPassword());
-            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE saltyfirm.firm SET firm_name = ?, firm_type = ?, overall_score = ?, description = ?, logo_url = ? WHERE firm_id = ?");
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE saltyfirm.firm SET firm_name = ?, firm_type = ?, description = ?, logo_url = ? WHERE firm_id = ?");
             preparedStatement.setString(1, firm.getFirmName());
             preparedStatement.setString(2, firm.getFirmType());
-            preparedStatement.setDouble(3, firm.getOverallScore());
-            preparedStatement.setString(4, firm.getDescription());
-            preparedStatement.setString(5, firm.getLogoURL());
+            preparedStatement.setString(3, firm.getDescription());
+            preparedStatement.setString(4, firm.getLogoURL());
+
 
 
         } catch (SQLException e){
