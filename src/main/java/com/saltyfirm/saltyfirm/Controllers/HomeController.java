5dfb5ff1d7483b25c76f1198686b46dc9e6e5e2a
cycloniccornet <patr5152@hotmail.com>
2019-12-11 11:38:02 +0100

    P mand

--- src/main/java/com/saltyfirm/saltyfirm/Controllers/HomeController.java
+++ src/main/java/com/saltyfirm/saltyfirm/Controllers/HomeController.java
@@ -4,6 +4,8 @@ import com.saltyfirm.saltyfirm.Models.Firm;
 import com.saltyfirm.saltyfirm.Models.User;
 import com.saltyfirm.saltyfirm.Services.FirmService;
 import com.saltyfirm.saltyfirm.Services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,8 @@ import java.util.List;
 
 @Controller
 public class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     UserService userService;
--- src/main/java/com/saltyfirm/saltyfirm/Test.java
+++ src/main/java/com/saltyfirm/saltyfirm/Test.java
@@ -13,21 +13,22 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
 
-        // UserRepositoryImpl ur = new UserRepositoryImpl();
-        // User user = new User("Frækfyr","1234", "Peter", "Sørensen", 76859485, "m", new Date(1985-01-02),"Leder","frækfyr@hotmail.com","Danmark","1");
-        // ur.createUser(user);
+         UserRepositoryImpl ur = new UserRepositoryImpl();
+         User user = new User("Frækfyr","1234", "Peter", "Sørensen", 76859485, "m", new Date(1985-01-02),"Leder","frækfyr@hotmail.com","Danmark","1");
+         ur.createUser(user);
+         System.out.println(user.getPrivileges());
         // System.out.println(ur.getAllUsers().toString());
         // User user1 = ur.findUserById(3);
         // user1.setFirstname("TESTER");
         // ur.editUser(user1);
         // System.out.println(ur.getAllUsers().toString());
 
-        DepartmentRepositoryImpl dr = new DepartmentRepositoryImpl();
-        System.out.println(dr.getAllReviews(2));
-        List<Department> departmentList = dr.getDepartments(2);
-        System.out.println(departmentList.toString());
-        dr.updateDepartmentScore(2);
-        System.out.println(dr.getDepartmentScores(2));
+        // DepartmentRepositoryImpl dr = new DepartmentRepositoryImpl();
+        // System.out.println(dr.getAllReviews(2));
+        // List<Department> departmentList = dr.getDepartments(2);
+        // System.out.println(departmentList.toString());
+        // dr.updateDepartmentScore(2);
+        // System.out.println(dr.getDepartmentScores(2));
         // dr.deleteDepartment(1);
         // List<Department> departmentList1 = dr.getDepartments(1);
         // System.out.println("Pension = "+dr.getDepartmentScores(1).get(0));
@@ -36,7 +37,7 @@ public class Test {
         // System.out.println("Environment = "+dr.getDepartmentScores(1).get(3));
         // System.out.println("Flexibility = "+dr.getDepartmentScores(1).get(4));
         // System.out.println("Total Score = "+dr.getDepartmentScores(1).get(5));
-        System.out.println(dr.findDepartmentById(2).toString());
+        // System.out.println(dr.findDepartmentById(2).toString());
 
         // FirmRepositoryImpl f = new FirmRepositoryImpl();
         // System.out.println(f.searchFirms("alle").toString());
