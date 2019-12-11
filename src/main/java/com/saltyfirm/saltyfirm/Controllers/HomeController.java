97f6beab49eee9b38009928a8f93b6c4f4ed1843
HolmQ84 <martin-holmqvist@hotmail.com>
2019-12-11 11:30:29 +0100

    Martins ændringer

--- src/main/java/com/saltyfirm/saltyfirm/Controllers/HomeController.java
+++ src/main/java/com/saltyfirm/saltyfirm/Controllers/HomeController.java
@@ -1,6 +1,8 @@
 package com.saltyfirm.saltyfirm.Controllers;
 
+import com.saltyfirm.saltyfirm.Models.Firm;
 import com.saltyfirm.saltyfirm.Models.User;
+import com.saltyfirm.saltyfirm.Services.FirmService;
 import com.saltyfirm.saltyfirm.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,11 +12,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
     @Autowired
     UserService userService;
+    @Autowired
+    FirmService firmService;
 
     @GetMapping("/")
     public String index1(Model model){
@@ -25,10 +31,10 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping("/{id}")
-    public String index2(@PathVariable int id, Model model){
-        User user = new User();
-        model.addAttribute(user);
+    @GetMapping("/{currentUserId}")
+    public String index2(@PathVariable int currentUserId, Model model){
+        User currentUser = userService.findUserById(currentUserId);
+        model.addAttribute(currentUser);
         String word = "";
         model.addAttribute(word);
         return "home";
@@ -43,6 +49,13 @@ public class HomeController {
         } else {
             return "redirect:/";
         }
+    }
+
+    @PostMapping("/{searchWord}")
+    public String searchIndex(@PathVariable String searchWord, Model model) {
+        List<Firm> searchResult = firmService.searchFirms(searchWord);
+        model.addAttribute(searchResult);
+        return "search";
     }
 
 
