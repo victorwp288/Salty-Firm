c176352b27330708d70908935ceda672ff6059fd
HolmQ84 <martin-holmqvist@hotmail.com>
2019-12-11 13:13:37 +0100

    Martins ændringer

---
+++ src/main/java/com/saltyfirm/saltyfirm/Models/SearchOverview.java
@@ -0,0 +1,37 @@
+package com.saltyfirm.saltyfirm.Models;
+
+public class SearchOverview {
+    private String firmName;
+    private String firmLogoUrl;
+    private double totalScore;
+
+    public SearchOverview(String firmName, String firmLogoUrl, double totalScore) {
+        this.firmName = firmName;
+        this.firmLogoUrl = firmLogoUrl;
+        this.totalScore = totalScore;
+    }
+
+    public String getFirmName() {
+        return firmName;
+    }
+
+    public void setFirmName(String firmName) {
+        this.firmName = firmName;
+    }
+
+    public String getFirmLogoUrl() {
+        return firmLogoUrl;
+    }
+
+    public void setFirmLogoUrl(String firmLogoUrl) {
+        this.firmLogoUrl = firmLogoUrl;
+    }
+
+    public double getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(double totalScore) {
+        this.totalScore = totalScore;
+    }
+}
