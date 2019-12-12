993d07ccad84cf5125a4d0153c1f08172e006e10
victorwp288 <victorwpetersen@gmail.com>
2019-12-12 10:58:13 +0100

    Updated shell

--- src/main/java/com/saltyfirm/saltyfirm/Controllers/FirmController.java
+++ src/main/java/com/saltyfirm/saltyfirm/Controllers/FirmController.java
@@ -1,7 +1,25 @@
 package com.saltyfirm.saltyfirm.Controllers;
 
+import com.saltyfirm.saltyfirm.Models.Firm;
+import com.saltyfirm.saltyfirm.Repositories.FirmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class FirmController {
+
+    @Autowired
+    FirmRepository firmRepository;
+
+    @GetMapping("/firms/{firmId}")
+    public String firms(@PathVariable int firmId, Model model){
+
+        Firm firm = firmRepository.findFirmById(firmId);
+        model.addAttribute("firms", firm);
+        return "firm";
+    }
+
 }
