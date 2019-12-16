afb1b7c992cc26438631ee58ee81a5be940a10b4
HolmQ84 <martin-holmqvist@hotmail.com>
2019-12-16 12:45:27 +0100

    Martins ændringer

--- src/main/java/com/saltyfirm/saltyfirm/Repositories/ReviewRepositoryImpl.java
+++ src/main/java/com/saltyfirm/saltyfirm/Repositories/ReviewRepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.saltyfirm.saltyfirm.Repositories;
 
-import com.saltyfirm.saltyfirm.Models.Firm;
 import com.saltyfirm.saltyfirm.Models.Review;
 import com.saltyfirm.saltyfirm.Repositories.DatabaseHelper.ProjectVariables;
 import org.springframework.stereotype.Service;
@@ -100,8 +99,8 @@ public class ReviewRepositoryImpl implements ReviewRepository {
             }
             resultSet.close();
             preparedStatement = connection.prepareStatement("INSERT INTO saltyfirm.review (post, salary, position, pension_scheme, benefits," +
-                    "management, work_environment, flexibility, employment_time, user_fk_id, department_fk_id) " +
-                    "VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+                                                                 "management, work_environment, flexibility, employment_time, user_fk_id, department_fk_id) " +
+                                                                 "VALUES (?,?,?,?,?,?,?,?,?,?,?)");
 
             preparedStatement.setString(1, review.getPost());
             preparedStatement.setInt(2, review.getSalary());
